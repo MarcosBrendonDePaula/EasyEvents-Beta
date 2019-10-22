98548014d321e43bed216c73ed80fab588b1f4ac
marcos brendon de paula azevedo <marcos.brendon@aluno.ufms.br>
2019-10-22 10:17:17 -0300

    Windows Threads
    Pthread removido para compatibilidade com a maioria dos compiladores presentes para windows

--- windows version optmized/Events.cpp
+++ windows version optmized/Events.cpp
@@ -8,7 +8,7 @@ Events::Events(){
     //this->Debug=Debug;
     this->rodando=0;
     Events::static_Acess=this;
-    pthread_create(&this->executor,0,Events::ExecutorF,this);
+    _beginthread(Events::ExecutorF,0,this);
 
 }
 
@@ -25,7 +25,7 @@ void Events::sendSignal(int ID){
     
 }
 
-void* Events::ExecutorF(void* arg){
+void Events::ExecutorF(void* arg){
     Events *This=(Events*)arg;
     	while(true){
 		lim:
@@ -42,9 +42,32 @@ void* Events::ExecutorF(void* arg){
 		This->LEDP.pop_front();
 	}
 }
+
+void Events::KeyMonitor(void* arg){
+    Events *This=(Events*)arg;
+    while (true) {						
+		Sleep(100);
+		for (int i = 8; i <= 255; i++) {
+			if (GetAsyncKeyState(i) == -32767) {
+                int *x = (int*)malloc(sizeof(int));
+                *x = i; 
+                This->getEvent(159753)->parametros = x;
+                This->sendSignal(159753);
+            }					
+		}
+    }
+}
+
 Event* Events::getEvent(int id){
 	return this->Leventos[id];
 }
+void Events::OnKeyMonitor(void(*funcao)(void*)){
+    if(!this->KeySMonitor){
+        this->addEvent(new Event(159753,funcao));
+        _beginthread(Events::KeyMonitor,0,this);
+    }
+}
+
 /*
     Construtor Evento
 */
@@ -52,4 +75,9 @@ Event::Event(int ID,void(*funcao)(void*),void *parametro){
     this->funcao=funcao;
     this->ID=ID;
     this->parametros=parametro;
+}
+Event::Event(int ID,void(*funcao)(void*)){
+    this->funcao=funcao;
+    this->ID=ID;
+    this->parametros=NULL;
 }
--- windows version optmized/Events.h
+++ windows version optmized/Events.h
@@ -1,28 +1,32 @@
 #include <iostream>
-#include <pthread.h>
 #include <map>
 #include <list>
+#include <process.h>
 #include <windows.h>
 #include <unistd.h>
 #include <stdio.h>
 class Event{
 public:
     Event(int ID,void(*funcao)(void*),void *parametro);
+    Event(int ID,void(*funcao)(void*));
+    Event(int ID,void(*funcao)());
     int ID;
     void *parametros;
     void(*funcao)(void*);
 };
 class Events{
 public:
-    static void* ExecutorF(void* arg);
+    static void ExecutorF(void* arg);
+    static void KeyMonitor(void* arg);
+    void OnKeyMonitor(void(*funcao)(void*));
     static Events *static_Acess;
     Events();
+    bool KeySMonitor=false;
     void addEvent(Event *E);
     void sendSignal(int ID);
 	Event* getEvent(int id);
 private:
     bool Debug;
-    pthread_t executor;
     std::list<int> LEDP;
     std::map<int,Event*> Leventos;
     bool rodando;

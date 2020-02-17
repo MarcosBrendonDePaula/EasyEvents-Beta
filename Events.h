d32f468782bf898f78ee5e26bfb9129f076f2eb6
Marcos Brendon De Paula <marcos.brendon@aluno.ufms.br>
2020-02-17 14:09:59 -0300

    Organização
    Removida as repetiçoes de codigo

--- Events.h
+++ Events.h
@@ -1,64 +1,49 @@
+#pragma once
 #ifdef __linux__
-    #include <iostream>
-    #include <pthread.h>
-    #include <map>
-    #include <list>
-    #include <unistd.h>
-    #include <stdio.h>
-    class Event{
+#include <iostream>
+#include <pthread.h>
+#elif _WIN32
+#include <process.h>
+#include <windows.h>
+#endif
+#include <map>
+#include <list>
+#include <unistd.h>
+#include <stdio.h>
+class Event
+{
     public:
         Event(int ID,void(*funcao)(void*),void *parametro);
+        Event(int ID,void(*funcao)(void*));
+        Event(int ID,void(*funcao)());
         int ID;
         void *parametros;
         void(*funcao)(void*);
-    };
-    class Events{
-    public:
-        static void* ExecutorF(void* arg);
-        static Events *static_Acess;
-        Events();
-        void addEvent(Event *E);
-        void sendSignal(int ID);
-    private:
-        bool Debug;
-        pthread_t executor;
-        std::list<int> LEDP;
-        std::map<int,Event*> Leventos;
-        bool rodando;
-    };
-#elif _WIN32
-#include <iostream>
-#include <map>
-#include <list>
-#include <process.h>
-#include <windows.h>
-#include <unistd.h>
-#include <stdio.h>
-class Event{
-public:
-    Event(int ID,void(*funcao)(void*),void *parametro);
-    Event(int ID,void(*funcao)(void*));
-    Event(int ID,void(*funcao)());
-    int ID;
-    void *parametros;
-    void(*funcao)(void*);
 };
-class Events{
+
+class Events
+{
     public:
+    #ifdef __linux__
+        static void* ExecutorF(void* arg);
+        static void* callFunction(void* arg);
+    #elif _WIN32
         static void ExecutorF(void* arg);
         static void KeyMonitor(void* arg);
         void OnKeyMonitor(void(*funcao)(void*));
-        static Events *static_Acess;
+        bool KeySMonitor = false;
+    #endif
         Events();
-        bool KeySMonitor=false;
         void addEvent(Event *E);
         void sendSignal(int ID);
-        bool Paralel;
         Event* getEvent(int id);
+        std::list<int> LEDP;
+        bool Paralel;
     private:
         bool Debug;
-        std::list<int> LEDP;
+    #ifdef __linux__
+        pthread_t executor;
+    #endif
         std::map<int,Event*> Leventos;
         bool rodando;
-    };
-#endif
+};
--- exemplo.cpp
+++ exemplo.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 #include "Events.h"
-void fun(void* arg){
-    int *x=(int*)arg;
-    int y=*x;
-    y++;
-    *x=y;
-    std::cout<<"Executou "<<*(x)<<std::endl;
+using namespace std;
+/*
+    Estrotura Basica para uma função Event
+    void Nome_função(void *arg)
+*/
+void fun(void* arg)
+{
+    string *variavel = (string*)arg;
+    cout<<*variavel<<endl;
 }
+
 int main(){
-    Events s;
-    int val=0;
-    int v2=7;
-    s.addEvent(new Event(0,&fun,&val));
-    s.addEvent(new Event(1,&fun,&v2));
-    s.sendSignal(0);
-    s.sendSignal(0);
-    Events::static_Acess->sendSignal(0);
-    Events::static_Acess->sendSignal(1);
+    //Criando objeto controlador de eventos
+    Events Evs;
+    /*  se setado como true Eventos serão desparados por threads 
+        caso false eventos serão executados sequencialmente.
+    */
+    Evs.Paralel = true;
+    //variavel para passar a fun
+    string sfun{"Ola mundo"};
+    //criando evento Event(ID_escolhida,endereço_função,parametros_a_passar)
+    Evs.addEvent(new Event(1,&fun,&sfun));
+    //disparando evento cujo a id seja 1 onde foi definida a cima.
+    Evs.sendSignal(1);
+    //pausando thread principal para esperara a execução dos eventos
     int x;
-    std::cin >> x;
+    cin>>x;
+    
     return 0;
 }
